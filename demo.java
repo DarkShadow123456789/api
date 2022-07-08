cd1d3ab5ba14e8583b3cadeb2657172b03843edc
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-07-08 11:56:06 +0530

    clear terminal

--- demo.java
+++ demo.java
@@ -29,6 +29,8 @@ class demo
     }
     public static void main(String[] args) 
     {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         try (Scanner sc = new Scanner(System.in)) 
         {
             whishme();
